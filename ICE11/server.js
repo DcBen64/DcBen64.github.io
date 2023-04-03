fcd3e73ad57d193bb86d09c58160065ffae61ece
Benjamin Walsh <benjamin.walsh1@dcmail.ca>
2023-04-02 22:45:07 -0400

    More ICE11

--- ICE11/server.js
+++ ICE11/server.js
@@ -6,8 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
 const fs_1 = __importDefault(require("fs"));
 const mime_types_1 = __importDefault(require("mime-types"));
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 3010;
+const hostname = '0.0.0.0';
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3010;
 let lookup = mime_types_1.default.lookup;
 const server = http_1.default.createServer((req, res) => {
     let path = req.url;
@@ -22,16 +22,13 @@ const server = http_1.default.createServer((req, res) => {
             res.end();
         }
         else {
+            res.setHeader("X-Content-Type-Options", "nosniff");
             res.writeHead(200, { 'Content-Type': mimeType });
-            res.write(data);
-            res.end();
+            res.end(data);
         }
-        res.setHeader("X-Content-Type-Options", "nosniff");
-        res.writeHead(200, { 'Content-Type': mimeType });
-        res.end(data);
     });
 });
-server.listen(port, () => {
+server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 //# sourceMappingURL=server.js.map
--- ICE11/server.ts
+++ ICE11/server.ts
@@ -2,9 +2,10 @@ import http from 'http';
 import fs from 'fs';
 import mime from 'mime-types';
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 3010;
+const hostname = '0.0.0.0';
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3010;
 let lookup = mime.lookup;
+
 const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
   let path = req.url as string;
 
@@ -20,16 +21,13 @@ const server = http.createServer((req: http.IncomingMessage, res: http.ServerRes
       res.write("<h1>Page Not Found</h1>");
       res.end();
     } else {
+      res.setHeader("X-Content-Type-Options", "nosniff");
       res.writeHead(200, { 'Content-Type': mimeType });
-      res.write(data);
-      res.end();
+      res.end(data);
     }
-    res.setHeader("X-Content-Type-Options", "nosniff")
-    res.writeHead(200, { 'Content-Type': mimeType });
-    res.end(data);
   });
 });
 
-server.listen(port, () => {
+server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
